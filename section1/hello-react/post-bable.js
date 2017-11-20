8db0db7b9767d50c56632612d7db402fd34d4c5c
jaywelborn <jesse.welborn@gmail.com>
2017-11-20 13:23:28 +0100

    hello world react codepen complete

---
+++ section1/hello-react/post-bable.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var element = React.createElement(
+  'h1',
+  null,
+  'Hello to reactjs'
+);
+var where = document.getElementById('root');
+
+ReactDOM.render(element, where);
---
+++ section1/hello-react/pre-babel.js
@@ -0,0 +1,4 @@
+ReactDOM.render(
+  <h1>Hello to reactjs</h1>,
+  document.getElementById('root')
+);
