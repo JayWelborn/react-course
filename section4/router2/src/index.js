f06741b54f3be2cda96799cd99b8fb8aa0343a5f
jaywelborn <jesse.welborn@gmail.com>
2017-11-27 20:50:32 +0100

    did router app2 video 1

---
+++ section4/router2/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import {
+  Router,
+  Route,
+  browserHistory
+} from 'react-router'
+
+ReactDOM.render(
+  <Router history={browserHistory}>
+    <Route path="/" component={App}/>
+  </Router>
+
+  , document.getElementById('root'));
+registerServiceWorker();
