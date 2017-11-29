1acc145987b3657ec98f1e4ee26e1d02026bf359
jaywelborn <jesse.welborn@gmail.com>
2017-11-29 21:24:33 +0100

    started section 5;

---
+++ section5/u-survey/src/Usurvey.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class Usurvey extends Component {
+  render() {
+    return (
+      <div>
+        I am from Usurvey Component.
+      </div>
+    );
+  }
+}
