faf1ac66efeaac4d7300a8d5a71e139874aa9b9a
jaywelborn <jesse.welborn@gmail.com>
2017-12-01 19:27:16 +0100

    added more questions, and event handling function outlines to log data to console on answer select and on submit

--- section5/u-survey/src/Usurvey.js
+++ section5/u-survey/src/Usurvey.js
@@ -22,6 +22,14 @@ export default class Usurvey extends Component {
     })
   }
 
+  answerSelected(event){
+    console.log(event.target.value)
+  }
+
+  questionSubmit(event){
+    console.log("submitted")
+  }
+
   constructor(props){
     super(props);
 
@@ -36,6 +44,8 @@ export default class Usurvey extends Component {
       isSubmitted: false
     };
     this.nameSubmit = this.nameSubmit.bind(this)
+    this.answerSelected = this.answerSelected.bind(this)
+    this.questionSubmit = this.questionSubmit.bind(this)
   }
 
   render() {
@@ -51,12 +61,38 @@ export default class Usurvey extends Component {
       </div>;
 
       questions = ''
-    } else if(this.state.studentName != '' && this.state.isSubmitted === false){
+    } else if(this.state.studentName !== '' && this.state.isSubmitted === false){
       studentName = <h1>Welcome to U-Survey {this.state.studentName}</h1>;
 
-      questions = <p>
-        This is a question
-      </p>
+      questions = <div>
+        <h2>Here are some questions:</h2>
+        <form onSubmit={this.questionSubmit}>
+          <div className="card">
+            <label>What kind of courses do you like?</label>
+            <br/>
+            <input type="radio" name="answer1" value="Technology" onChange={this.answerSelected}/>Technology
+            <input type="radio" name="answer1" value="Design" onChange={this.answerSelected}/>Design
+            <input type="radio" name="answer1" value="Marketing" onChange={this.answerSelected}/>Marketing
+          </div>
+          <div className="card">
+            <label>You are a: </label>
+            <br/>
+            <input type="radio" name="answer2" value="Student" onChange={this.answerSelected}/>Student
+            <input type="radio" name="answer2" value="in-job" onChange={this.answerSelected}/>in-job
+            <input type="radio" name="answer2" value="looking-job" onChange={this.answerSelected}/>looking-job
+          </div>
+          <div className="card">
+            <label>Is online learning helpful? </label>
+            <br/>
+            <input type="radio" name="answer3" value="yes" onChange={this.answerSelected}/>yes
+            <input type="radio" name="answer3" value="no" onChange={this.answerSelected}/>no
+            <input type="radio" name="answer3" value="maybe" onChange={this.answerSelected}/>maybe
+          </div>
+          <input className="feedback-button" type="submit" value="Submit"/>
+        </form>
+      </div>
+    } else if(this.state.studentname !== '' && this.state.isSubmitted === true){
+      studentName = <h1>Thanks, {this.state.studentName}</h1>
     }
     return (
       <div>
