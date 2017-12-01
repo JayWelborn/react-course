ddea2850e7e1d68789a7682c20834b2cfed0b0a2
jaywelborn <jesse.welborn@gmail.com>
2017-12-01 19:06:09 +0100

    added display user name on form submission

--- section5/u-survey/src/Usurvey.js
+++ section5/u-survey/src/Usurvey.js
@@ -14,12 +14,20 @@ var config = {
 firebase.initializeApp(config);
 
 export default class Usurvey extends Component {
+
+  nameSubmit(event){
+    var studentName = this.refs.name.value
+    this.setState({studentName: studentName}, function(){
+      console.log(this.state)
+    })
+  }
+
   constructor(props){
     super(props);
 
     this.state = {
       uid: uuid.v1(),
-      studentName: '',
+      studentName: 'killerMan',
       answers: {
         answer1: '',
         answer2: '',
@@ -27,6 +35,7 @@ export default class Usurvey extends Component {
       },
       isSubmitted: false
     };
+    this.nameSubmit = this.nameSubmit.bind(this)
   }
 
   render() {
@@ -36,10 +45,18 @@ export default class Usurvey extends Component {
     if(this.state.studentName === '' && this.state.isSubmitted === false){
       studentName = <div>
         <h1>Hey Student, please tell us your name.</h1>
-        <form>
-          <input type="text" placeholder="Enter your name" ref="name"/>
+        <form onSubmit={this.nameSubmit}>
+          <input className="namy" type="text" placeholder="Enter your name" ref="name"/>
         </form>
-      </div>
+      </div>;
+
+      questions = ''
+    } else if(this.state.studentName != '' && this.state.isSubmitted === false){
+      studentName = <h1>Welcome to U-Survey {this.state.studentName}</h1>;
+
+      questions = <p>
+        This is a question
+      </p>
     }
     return (
       <div>
