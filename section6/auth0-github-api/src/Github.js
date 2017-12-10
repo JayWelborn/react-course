93b23aec67367b3893231c119e7d7e286d54bf7c
jaywelborn <jesse.welborn@gmail.com>
2017-12-10 22:05:38 +0100

    started github auth project

---
+++ section6/auth0-github-api/src/Github.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class Github extends Component {
+  render() {
+    return (
+      <div>
+        <h1>I am from Github</h1>
+      </div>
+    );
+  }
+}
