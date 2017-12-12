983708b9467918f21f34c731c42daa766fe4a4f9
jaywelborn <jesse.welborn@gmail.com>
2017-12-12 17:33:55 +0100

    continued youtube-fetch, moved keys to untracked file

--- youtube-fetch/src/Youtube.js
+++ youtube-fetch/src/Youtube.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import Keys from './Keys'
+
+const YOUTUBE_API_KEY = Keys.youtube_api;
+const channelID = Keys.youtube_channel;
 
 
 export default class Youtube extends Component {
   render() {
     return (
       <div>
-        I am from Youtube Component
+        <button>Get YouTube Videos</button>
+        <div className="youtube">
+
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/85VCxpO7E2A"
+            frameBorder="0" gesture="media" allow="encrypted-media"
+            allowFullScreen></iframe>
+        </div>
       </div>
     );
   }
